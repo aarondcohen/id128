9cfc8986745732bf372162c6abff2a12d58ee30e
Aaron Cohen <aarondcohen@gmail.com>
2018-01-24 00:35:55 -0500

    Add benchmark for basic operations

---
+++ benchmark/basics.js
@@ -0,0 +1,24 @@
+const Id128 = require('../');
+
+[
+	'Ulid',
+	'UlidMonotonic',
+	'Uuid4',
+	'UuidNil',
+].forEach((id_type_name) => suite(id_type_name, function() {
+  set('iterations', 100000);
+
+	const id_type = Id128[id_type_name];
+	const id = id_type.generate();
+	const canonical = id.toCanonical();
+	const raw = id.toRaw();
+
+	bench('generate', () => id_type.generate());
+	bench('fromCanonical', () => id_type.fromCanonical(canonical));
+	bench('fromCanonicalTrusted', () => id_type.fromCanonicalTrusted(canonical));
+	bench('fromRaw', () => id_type.fromRaw(raw));
+	bench('fromRawTrusted', () => id_type.fromRawTrusted(raw));
+	bench('toCanonical', () => id.toCanonical());
+	bench('toRaw', () => id.toRaw());
+}));
+
