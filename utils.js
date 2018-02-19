2912b7bf3baa4967e6b56d59b2949bb4bf80521b
Aaron Cohen <aarondcohen@gmail.com>
2018-02-19 02:08:13 -0500

    Let developers configure their own id factories

--- utils.js
+++ utils.js
@@ -7,6 +7,7 @@ const Crockford32Coder = require('./src/coder/crockford32');
 const HexCoder = require('./src/coder/hex');
 const UuidCoder = require('./src/coder/uuid');
 
+const { IdFactory } = require('./src/factory/id');
 const Exception = require('./src/common/exception');
 
 module.exports = {
@@ -22,4 +23,5 @@ module.exports = {
 		Uuid4,
 		UuidNil,
 	},
+	IdFactory,
 };
