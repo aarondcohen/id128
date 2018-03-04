55cc659cbf226541b7bbde620a53258fc6bbee0e
Aaron Cohen <aarondcohen@gmail.com>
2018-03-03 19:28:40 -0500

    Replace uint-converter with hand optimizations
    - replaced due to the overhead of the loop vs bit math

--- src/id/ulid.js
+++ src/id/ulid.js
@@ -1,21 +1,25 @@
 const ByteArray = require('../common/byte-array');
 const EpochConverter = require('../common/epoch-converter');
-const UintConverter = require('../common/uint-converter');
 const { BaseId } = require('./base');
 
 const TIME_OFFSET = 0;
-const RANDOM_OFFSET = 6;
 
 const EPOCH_ORIGIN_MS = 0;
+const UINT32_RADIX = Math.pow(2, 32);
+const UINT8_MAX = 0b11111111;
 
 function setTime(time, bytes) {
-	UintConverter.assignUint(
-		TIME_OFFSET,
-		RANDOM_OFFSET,
-		bytes,
-		time
-	);
-};
+	const time_low = time % UINT32_RADIX;
+	const time_high = (time - time_low) / UINT32_RADIX;
+
+	let idx = TIME_OFFSET - 1;
+	bytes[++idx] = (time_high >>> 8) & UINT8_MAX;
+	bytes[++idx] = (time_high >>> 0) & UINT8_MAX;
+	bytes[++idx] = (time_low >>> 24) & UINT8_MAX;
+	bytes[++idx] = (time_low >>> 16) & UINT8_MAX;
+	bytes[++idx] = (time_low >>> 8) & UINT8_MAX;
+	bytes[++idx] = (time_low >>> 0) & UINT8_MAX;
+}
 
 class Ulid extends BaseId {
 
@@ -42,11 +46,17 @@ class Ulid extends BaseId {
 	// Accessors
 
 	get time() {
-		const epoch_ms = UintConverter.extractUint(
-			TIME_OFFSET,
-			RANDOM_OFFSET,
-			this.bytes
-		);
+		let idx = TIME_OFFSET - 1;
+		const time_high = 0
+			| (this.bytes[++idx] << 8)
+			| (this.bytes[++idx] << 0);
+		const time_low = 0
+			| (this.bytes[++idx] << 24)
+			| (this.bytes[++idx] << 16)
+			| (this.bytes[++idx] << 8)
+			| (this.bytes[++idx] << 0);
+		const epoch_ms = (time_high * UINT32_RADIX) + (time_low >>> 0);
+
 		return EpochConverter.fromEpoch(EPOCH_ORIGIN_MS, epoch_ms);
 	};
 }
