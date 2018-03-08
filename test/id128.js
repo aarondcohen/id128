c5e4620beddc2094a7c06bd9d510d0e182529fda
Aaron Cohen <aarondcohen@gmail.com>
2018-03-08 01:40:14 -0500

    Add Uuid to decode and generate versioned UUIDs
    - add Uuid to Id128
    - add benchmarks for UUIDs accessed through the generic factory

--- test/id128.js
+++ test/id128.js
@@ -15,110 +15,108 @@ function assertDebuggable(id_name, generator) {
 	});
 }
 
-function assertValidId128(id_name, factory, id_class) {
-	describe(`${id_name} Factory`, function() {
-		describe('new', function() {
-			it('is disabled', function() {
-				expect(() => new factory).to.throw(TypeError);
+function assertValidId128(id_name, factory, id_class, generator_args = {}) {
+	describe('new', function() {
+		it('is disabled', function() {
+			expect(() => new factory).to.throw(TypeError);
+		});
+	});
+
+	describe('.generate', function() {
+		const subject = () => factory.generate(generator_args);
+
+		it(`returns a ${id_name}`, function() {
+			expect(subject()).to.be.an.instanceof(id_class);
+		});
+
+		it('generates 128-bit id', function() {
+			expect(subject().bytes).to.have.lengthOf(16);
+		});
+
+		assertDebuggable(id_name, subject);
+	});
+
+	describe('.MIN', function() {
+		const subject = () => factory.MIN(generator_args);
+
+		it(`returns a ${id_name}`, function() {
+			expect(subject()).to.be.an.instanceof(id_class);
+		});
+
+		it('generates 128-bit id', function() {
+			expect(subject().bytes).to.have.lengthOf(16);
+		});
+
+		assertDebuggable(id_name, subject);
+	});
+
+	describe('.MAX', function() {
+		const subject = () => factory.MAX(generator_args);
+
+		it(`returns a ${id_name}`, function() {
+			expect(subject()).to.be.an.instanceof(id_class);
+		});
+
+		it('generates 128-bit id', function() {
+			expect(subject().bytes).to.have.lengthOf(16);
+		});
+
+		assertDebuggable(id_name, subject);
+	});
+
+	describe('canonical', function() {
+		const id = factory.generate(generator_args);
+
+		it('encodes to a string', function() {
+			expect(factory.toCanonical(id)).to.be.a('string');
+		});
+
+		it(`decodes to a ${id_name}`, function() {
+			expect(factory.fromCanonical(factory.toCanonical(id)))
+				.to.be.an.instanceOf(id_class);
+		});
+
+		it('converts symmetrically', function() {
+			[
+				['generated', id],
+				['min', factory.MIN(generator_args)],
+				['max', factory.MAX(generator_args)],
+			].forEach(([label, test_id]) => {
+				expect(factory.fromCanonical(factory.toCanonical(test_id)), label)
+					.to.deep.equal(test_id);
 			});
 		});
 
-		describe('.generate', function() {
-			const subject = () => factory.generate();
+		describe('when decoded', function() {
+			assertDebuggable(id_name, () => factory.fromCanonical(id.toCanonical()));
+		});
+	});
 
-			it(`returns a ${id_name}`, function() {
-				expect(subject()).to.be.an.instanceof(id_class);
-			});
+	describe('raw', function() {
+		const id = factory.generate(generator_args);
 
-			it('generates 128-bit id', function() {
-				expect(subject().bytes).to.have.lengthOf(16);
-			});
-
-			assertDebuggable(id_name, subject);
+		it('encodes to a string', function() {
+			expect(factory.toRaw(id)).to.be.a('string');
 		});
 
-		describe('.MIN', function() {
-			const subject = () => factory.MIN();
-
-			it(`returns a ${id_name}`, function() {
-				expect(subject()).to.be.an.instanceof(id_class);
-			});
-
-			it('generates 128-bit id', function() {
-				expect(subject().bytes).to.have.lengthOf(16);
-			});
-
-			assertDebuggable(id_name, subject);
+		it(`decodes to a ${id_name}`, function() {
+			expect(factory.fromRaw(factory.toRaw(id)))
+				.to.be.an.instanceOf(id_class);
 		});
 
-		describe('.MAX', function() {
-			const subject = () => factory.MAX();
-
-			it(`returns a ${id_name}`, function() {
-				expect(subject()).to.be.an.instanceof(id_class);
-			});
-
-			it('generates 128-bit id', function() {
-				expect(subject().bytes).to.have.lengthOf(16);
-			});
-
-			assertDebuggable(id_name, subject);
-		});
-
-		describe('canonical', function() {
-			const id = factory.generate();
-
-			it('encodes to a string', function() {
-				expect(factory.toCanonical(id)).to.be.a('string');
-			});
-
-			it(`decodes to a ${id_name}`, function() {
-				expect(factory.fromCanonical(factory.toCanonical(id)))
-					.to.be.an.instanceOf(id_class);
-			});
-
-			it('converts symmetrically', function() {
-				[
-					['generated', id],
-					['min', factory.MIN()],
-					['max', factory.MAX()],
-				].forEach(([label, test_id]) => {
-					expect(factory.fromCanonical(factory.toCanonical(test_id)), label)
-						.to.deep.equal(test_id);
-				});
-			});
-
-			describe('when decoded', function() {
-				assertDebuggable(id_name, () => factory.fromCanonical(id.toCanonical()));
+		it('converts symmetrically', function() {
+			[
+				['generated', id],
+				['min', factory.MIN(generator_args)],
+				['max', factory.MAX(generator_args)],
+			].forEach(([label, test_id]) => {
+				expect(factory.fromRaw(factory.toRaw(test_id)), label)
+					.to.deep.equal(test_id);
 			});
 		});
 
-		describe('raw', function() {
-			const id = factory.generate();
-
-			it('encodes to a string', function() {
-				expect(factory.toRaw(id)).to.be.a('string');
-			});
-
-			it(`decodes to a ${id_name}`, function() {
-				expect(factory.fromRaw(factory.toRaw(id)))
-					.to.be.an.instanceOf(id_class);
-			});
-
-			it('converts symmetrically', function() {
-				[
-					['generated', id],
-					['min', factory.MIN()],
-					['max', factory.MAX()],
-				].forEach(([label, test_id]) => {
-					expect(factory.fromRaw(factory.toRaw(test_id)), label)
-						.to.deep.equal(test_id);
-				});
-			});
-
-			describe('when decoded', function() {
-				assertDebuggable(id_name, () => factory.fromRaw(id.toRaw()));
-			});
+		describe('when decoded', function() {
+			assertDebuggable(id_name, () => factory.fromRaw(id.toRaw()));
 		});
 	});
 }
@@ -129,9 +127,25 @@ function assertValidId128(id_name, factory, id_class) {
 	'Uuid1',
 	'Uuid4',
 	'UuidNil',
-].forEach((id_name) => assertValidId128(
-	id_name,
-	Id128[id_name],
-	Id[id_name],
-));
+].forEach((id_name) => describe(`${id_name} Factory`, function() {
+	assertValidId128(
+		id_name,
+		Id128[id_name],
+		Id[id_name],
+	);
+}));
 
+[
+	['Uuid1', { version: 1 }],
+	['Uuid4', { version: 4 }],
+	['UuidNil', { version: 0 }],
+].forEach(([id_name, generator_args]) => {
+	describe(`Uuid Factory generating ${id_name}`, function() {
+		assertValidId128(
+			id_name,
+			Id128.Uuid,
+			Id[id_name],
+			generator_args,
+		);
+	});
+});
