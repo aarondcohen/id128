121e01f5438d48470bcf452d1d8acf23e108b5bd
Aaron Cohen <aarondcohen@gmail.com>
2018-03-09 08:38:21 -0500

    Export Uuid6 as a Uuid
    - update documentation
    - also add VersionedIdFactory to Utils

--- utils.js
+++ utils.js
@@ -1,7 +1,9 @@
 const { Ulid } = require('./src/id/ulid');
 const { UlidMonotonic } = require('./src/id/ulid-monotonic');
+const { Uuid } = require('./src/id/uuid');
 const { Uuid1 } = require('./src/id/uuid-1');
 const { Uuid4 } = require('./src/id/uuid-4');
+const { Uuid6 } = require('./src/id/uuid-6');
 const { UuidNil } = require('./src/id/uuid-nil');
 
 const Crockford32Coder = require('./src/coder/crockford32');
@@ -9,6 +11,7 @@ const HexCoder = require('./src/coder/hex');
 const UuidCoder = require('./src/coder/uuid');
 
 const { IdFactory } = require('./src/factory/id');
+const { VersionedIdFactory } = require('./src/factory/versioned-id');
 const Exception = require('./src/common/exception');
 
 module.exports = {
@@ -21,9 +24,12 @@ module.exports = {
 	Id: {
 		Ulid,
 		UlidMonotonic,
+		Uuid,
 		Uuid1,
 		Uuid4,
+		Uuid6,
 		UuidNil,
 	},
 	IdFactory,
+	VersionedIdFactory,
 };
