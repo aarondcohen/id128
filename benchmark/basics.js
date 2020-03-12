704582ef80a92d85473372ac44289d11667ae690
Aaron Cohen <aarondcohen@gmail.com>
2020-03-12 03:24:48 -0400

    Switch to supported benchmark framework
    - remove matcha
    - add benchmarkify
    - update README stats
    - clarify UlidMonotonic corner cases

--- benchmark/basics.js
+++ benchmark/basics.js
@@ -1,25 +1,40 @@
+const Benchmarkify = require('benchmarkify');
 const Id128 = require('../');
 const { IdFactory } = require('../utils');
 const { VersionedIdFactory } = require('../utils');
+
+const benchmark = new Benchmarkify("Basics").printHeader();
+const suites_by_name = Object.fromEntries([
+	'generate',
+	'MIN',
+	'MAX',
+	'fromCanonical',
+	'fromCanonicalTrusted',
+	'fromRaw',
+	'fromRawTrusted',
+	'toCanonical',
+	'toRaw'
+].map((name) => [name, benchmark.createSuite(name, { cycles: 100000 })]));
 
 Object.values(Id128)
 	.filter(x => x instanceof IdFactory)
 	.filter(x => !(x instanceof VersionedIdFactory))
 	.sort((l, r)  => l.name.localeCompare(r.name))
-	.forEach((id_type) => suite(id_type.name, function() {
-		set('iterations', 1000000);
-
+	.forEach((id_type) => {
+		const name = id_type.name;
 		const id = id_type.generate();
 		const canonical = id.toCanonical();
 		const raw = id.toRaw();
 
-		bench('generate', () => id_type.generate());
-		bench('MIN', () => id_type.MIN());
-		bench('MAX', () => id_type.MAX());
-		bench('fromCanonical', () => id_type.fromCanonical(canonical));
-		bench('fromCanonicalTrusted', () => id_type.fromCanonicalTrusted(canonical));
-		bench('fromRaw', () => id_type.fromRaw(raw));
-		bench('fromRawTrusted', () => id_type.fromRawTrusted(raw));
-		bench('toCanonical', () => id.toCanonical());
-		bench('toRaw', () => id.toRaw());
-	}));
+		suites_by_name.generate.add(name, () => id_type.generate());
+		suites_by_name.MIN.add(name, () => id_type.MIN());
+		suites_by_name.MAX.add(name, () => id_type.MAX());
+		suites_by_name.fromCanonical.add(name, () => id_type.fromCanonical(canonical));
+		suites_by_name.fromCanonicalTrusted.add(name, () => id_type.fromCanonicalTrusted(canonical));
+		suites_by_name.fromRaw.add(name, () => id_type.fromRaw(raw));
+		suites_by_name.fromRawTrusted.add(name, () => id_type.fromRawTrusted(raw));
+		suites_by_name.toCanonical.add(name, () => id.toCanonical());
+		suites_by_name.toRaw.add(name, () => id.toRaw());
+	});
+
+benchmark.run(Object.values(suites_by_name));
--- benchmark/factory-uuid.js
+++ benchmark/factory-uuid.js
@@ -1,21 +1,34 @@
+const Benchmarkify = require('benchmarkify');
 const Id128 = require('../');
+
+const benchmark = new Benchmarkify("UUID Processing").printHeader();
+const suites_by_name = Object.fromEntries([
+	'generate',
+	'MIN',
+	'MAX',
+	'fromCanonical',
+	'fromCanonicalTrusted',
+	'fromRaw',
+	'fromRawTrusted',
+].map((name) => [name, benchmark.createSuite(name, { cycles: 100000 })]));
 
 const factory = Id128.Uuid;
 factory.versioned_ids
 	.sort((l, r)  => l.name.localeCompare(r.name))
-	.forEach((id_type) => suite(`Uuid processing ${id_type.name}`, function() {
-		set('iterations', 1000000);
-
+	.forEach((id_type) => {
+		const name = id_type.name;
 		const id = id_type.generate();
 		const canonical = id.toCanonical();
 		const raw = id.toRaw();
 		const type = { version: id.version };
 
-		bench('generate', () => factory.generate(type));
-		bench('MIN', () => factory.MIN(type));
-		bench('MAX', () => factory.MAX(type));
-		bench('fromCanonical', () => factory.fromCanonical(canonical));
-		bench('fromCanonicalTrusted', () => factory.fromCanonicalTrusted(canonical));
-		bench('fromRaw', () => factory.fromRaw(raw));
-		bench('fromRawTrusted', () => factory.fromRawTrusted(raw));
-	}));
+		suites_by_name.generate.add(name, () => factory.generate(type));
+		suites_by_name.MIN.add(name, () => factory.MIN(type));
+		suites_by_name.MAX.add(name, () => factory.MAX(type));
+		suites_by_name.fromCanonical.add(name, () => factory.fromCanonical(canonical));
+		suites_by_name.fromCanonicalTrusted.add(name, () => factory.fromCanonicalTrusted(canonical));
+		suites_by_name.fromRaw.add(name, () => factory.fromRaw(raw));
+		suites_by_name.fromRawTrusted.add(name, () => factory.fromRawTrusted(raw));
+	});
+
+benchmark.run(Object.values(suites_by_name));
