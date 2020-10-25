920962f8cd03c01c92dce71b6186ed2b7e1def5b
Aaron Cohen <aarondcohen@gmail.com>
2020-10-25 18:21:24 -0400

    Tweaked random bytes allocation for performance
    - server-side: allocate a full page of random, minus the buffer overhead
    - added script for simpler benchmark generation
    - upgraded benchmarkify for consistency issues
    - update benchmark results

--- src/common/random-bytes.js
+++ src/common/random-bytes.js
@@ -1,6 +1,6 @@
 const Crypto = require('crypto');
 
-const MAX_BUFFER = 512;
+const MAX_BUFFER = 4096 /* typical page size */ - 96 /* Empty buffer overhead */;
 
 let buffer;
 let offset = MAX_BUFFER;
