238f38f3ff6e61c93ceb898b061c2dac2ca7193a
Roel Berger <berger.roel@gmail.com>
2021-01-24 11:39:34 +0000

    Support for running in workers

--- src/common/random-bytes-browser.js
+++ src/common/random-bytes-browser.js
@@ -1,4 +1,15 @@
-const Crypto = window.crypto;
+function resolveCrypto() {
+	if(typeof window !== 'undefined' && window.crypto != null) {
+		// UI thread
+		return window.crypto;
+	} else if (typeof self !== 'undefined' && self.crypto != null) {
+		// Worker thread
+		return self.crypto;
+	} else {
+		throw new Error('crypto not found');
+	}
+}
+const Crypto = resolveCrypto();
 
 function randomBytes(size) {
 	const bytes = new Uint8Array(size);
