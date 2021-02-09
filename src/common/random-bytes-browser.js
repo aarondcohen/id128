eaeb57adf7b57a5888a59e5016b99b839f3f04c0
Roel Berger <berger.roel@gmail.com>
2021-02-09 08:23:00 +0000

    Use self instead of window to resolve crypto

--- src/common/random-bytes-browser.js
+++ src/common/random-bytes-browser.js
@@ -1,15 +1,4 @@
-function resolveCrypto() {
-	if(typeof window !== 'undefined' && window.crypto != null) {
-		// UI thread
-		return window.crypto;
-	} else if (typeof self !== 'undefined' && self.crypto != null) {
-		// Worker thread
-		return self.crypto;
-	} else {
-		throw new Error('crypto not found');
-	}
-}
-const Crypto = resolveCrypto();
+const Crypto = self.crypto;
 
 function randomBytes(size) {
 	const bytes = new Uint8Array(size);
